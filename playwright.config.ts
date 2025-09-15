41bd37427693bc6100c1f80ad8292e2219e74294
janpantest <krchovaak@gmail.com>
2025-09-15 12:51:48 +0200

    allure report for BE

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,9 +3,17 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   use: {
     baseURL: 'https://demoqa.com',
+    trace: 'on-first-retry',  // optional, keeps trace for failed tests
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
   // Optional: set global timeout, retries, etc.
   timeout: 30_000,
   retries: 1,
-  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+  // reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+
+  reporter: [
+      ['list'],                  // Console output
+      ['allure-playwright'],     // Allure reporter
+  ],
 });
